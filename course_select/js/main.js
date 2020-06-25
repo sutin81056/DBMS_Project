5aef579b65de2698d1e9be335729928f8fe1a7a3
katie5413 <katie87827@gmail.com>
2020-06-25 23:34:36 +0800

    連動選單完成
    資料有夠多ＱＱ

--- course_select/js/main.js
+++ course_select/js/main.js
@@ -118,33 +118,108 @@ function dataHierarchy() {
     //輔系 學分專班
     var minor_credit_program = new Array();
     var i = 0;
-    minor_credit_program[i++] = new node("輔系專班", ["中輔", "外輔", "貿輔", "會輔", "英輔", "日輔", "韓輔", "法輔"]);
-    minor_credit_program[i++] = new node("學分學程專班", ["台灣與近代世界學程 ", "文化、 傳播與現代社會學程 ", "創意學程 ", "華語文教學學分學程 ", "人文學程 ", "中國大陸研究跨領域學程 ", "東北亞語言與文化學程 ", "斯拉夫語文學程 ", "中東語言與文化學程 ", "調查研究方法菁英學分學程 ", "社會科學榮譽學程 ", "外文中心歐洲語言與文化學程 ", "國際事務學院涉外事務與談判學程 ", "外語傳播第二專長學程 ", "英語商管學程 ", "英語文榮譽學程 ", "人文榮譽學程 ", "法律與會計碩士學程 ", "藝術產業經營學程 ", " 生物科技管理學程 ", "台灣人文數位典藏與應用學程 ", "東南亞語文學分學程 ", "性別研究跨領域學程 ", "日本研究碩士學分學程 ", "專利學分學程 ", "歐洲聯盟研究碩士學分學程 ", "全球政府採購學分學程 ", "翻譯與跨文化學分學程 ", "供應鏈管理學分學程 ", "數理財務學分學程 ", "語言、 認知與大腦 ", "社會創新領袖學分學程 ", "博雅榮譽學分學程 ", "外語專長商管學分學程 ", "創意設計實務學分學程 ", "電子物理學分學程 ", "國際經貿談判人才培育跨校學分學程 ", "國際事務學院英語授課學分學程 ", "傳播學院國際傳播英語學分學程 ", "社會科學學院英語授課學分學程 ", "法學院英語授課學分學程 ", "理學院英語授課學分學程 ", "教育學院英語授課學分學程 ", "外國語文學院世界文化英語學分學程 ", "文學院英語學分學程 ", "大腦科學學分學程 ", "日本文化與經濟學分學程 ", "人文創新學程 ", "全球治理英語學分學程 "]);
-    minor_credit_program[i++] = new node("教學實習與實務", []);
+    minor_credit_program[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    minor_credit_program[i++] = new node("輔系專班", ["-----請選擇Choose-----", "中輔", "外輔", "貿輔", "會輔", "英輔", "日輔", "韓輔", "法輔"]);
+    minor_credit_program[i++] = new node("學分學程專班", ["-----請選擇Choose-----", "台灣與近代世界學程 ", "文化、 傳播與現代社會學程 ", "創意學程 ", "華語文教學學分學程 ", "人文學程 ", "中國大陸研究跨領域學程 ", "東北亞語言與文化學程 ", "斯拉夫語文學程 ", "中東語言與文化學程 ", "調查研究方法菁英學分學程 ", "社會科學榮譽學程 ", "外文中心歐洲語言與文化學程 ", "國際事務學院涉外事務與談判學程 ", "外語傳播第二專長學程 ", "英語商管學程 ", "英語文榮譽學程 ", "人文榮譽學程 ", "法律與會計碩士學程 ", "藝術產業經營學程 ", " 生物科技管理學程 ", "台灣人文數位典藏與應用學程 ", "東南亞語文學分學程 ", "性別研究跨領域學程 ", "日本研究碩士學分學程 ", "專利學分學程 ", "歐洲聯盟研究碩士學分學程 ", "全球政府採購學分學程 ", "翻譯與跨文化學分學程 ", "供應鏈管理學分學程 ", "數理財務學分學程 ", "語言、 認知與大腦 ", "社會創新領袖學分學程 ", "博雅榮譽學分學程 ", "外語專長商管學分學程 ", "創意設計實務學分學程 ", "電子物理學分學程 ", "國際經貿談判人才培育跨校學分學程 ", "國際事務學院英語授課學分學程 ", "傳播學院國際傳播英語學分學程 ", "社會科學學院英語授課學分學程 ", "法學院英語授課學分學程 ", "理學院英語授課學分學程 ", "教育學院英語授課學分學程 ", "外國語文學院世界文化英語學分學程 ", "文學院英語學分學程 ", "大腦科學學分學程 ", "日本文化與經濟學分學程 ", "人文創新學程 ", "全球治理英語學分學程 "]);
+    minor_credit_program[i++] = new node("教學實習與實務", ["教學實習與實務"]);
 
+    //服務、體育、國防
+    var service_PE_all_out_defense = new Array();
+    var i = 0;
+    service_PE_all_out_defense[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    service_PE_all_out_defense[i++] = new node("服務學習", ["服務學習"]);
+    service_PE_all_out_defense[i++] = new node("體育", ["-----請選擇Choose-----", "體育必修 ", "體育選修 ", "體育代表隊／特別班 "]);
+    service_PE_all_out_defense[i++] = new node("全民國防", ["全民國防"]);
+
+    //文
+    var liberal = new Array();
+    var i = 0;
+    liberal[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    liberal[i++] = new node("學士班", ["-----請選擇Choose-----", "中國文學系", "歷史學系", "哲學系", "人文榮譽學程"]);
+    liberal[i++] = new node("碩士班", ["-----請選擇Choose-----", "中國文學系", "歷史學系", "哲學系", "圖書資訊與檔案學研究所", "宗教研究所", "台灣史研究所", "台灣文學研究所", "華語文教學碩士學位學程", "國文教學碩士在職專班", "圖書資訊學數位碩士在職專班"]);
+    liberal[i++] = new node("博士班", ["-----請選擇Choose-----", "中國文學系", "歷史學系", "哲學系", "圖書資訊與檔案學研究所", "宗教研究所", "台灣史研究所", "台灣文學研究所", "華語文教學博士學位學程"]);
+
+
+    //社科
+    var social_science = new Array();
+    var i = 0;
+    social_science[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    social_science[i++] = new node("學士班", ["-----請選擇Choose-----", "政治學系", "財政學系", "社會學系", "公共行政學系", "地政學系", "經濟學系", "民族學系"]);
+    social_science[i++] = new node("碩士班", ["-----請選擇Choose-----", "政治學系", "財政學系", "社會學系", "公共行政學系", "地政學系", "經濟學系", "民族學系", "國家發展研究所", "勞工研究所", "社會工作研究所", "行政管理碩士學程", "地政學系碩士在職專班", "亞太研究英語碩士學程"]);
+    social_science[i++] = new node("博士班", ["-----請選擇Choose-----", "政治學系", "財政學系", "社會學系", "公共行政學系", "地政學系", "經濟學系", "民族學系", "國家發展研究所", "社會工作研究所", "亞太研究英語博士學程"]);
+
+    //商
+    var commerse = new Array();
+    var i = 0;
+    commerse[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    commerse[i++] = new node("學士班", ["-----請選擇Choose-----", "商學院", "國際經營與貿易系", "金融學系", "會計學系", "統計學系", "企業管理", "資訊管理學系", "財務管理學系", "風險管理與保險學系"]);
+    commerse[i++] = new node("碩士班", ["-----請選擇Choose-----", "國際經營與貿易系", "金融學系", "會計學系", "統計學系", "企業管理", "資訊管理學系", "財務管理學系", "風險管理與保險學系"]); //對不起我累了，有空再補
+    commerse[i++] = new node("博士班", ["-----請選擇Choose-----", "國際經營與貿易系", "金融學系", "會計學系", "統計學系", "企業管理", "資訊管理學系", "財務管理學系", "風險管理與保險學系"]); //對不起我累了，有空再補
+
+    //傳播
+    var communication = new Array();
+    var i = 0;
+    communication[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    communication[i++] = new node("學士班", ["-----請選擇Choose-----", "傳播學院前段不分班課程", "傳播學院大一大二不分系", "新聞學系", "廣告學系", "廣播電視學系", "傳播學院傳播學士學位學程"]);
+    communication[i++] = new node("碩士班", ["-----請選擇Choose-----", "新聞學系", "廣告學系", "廣播電視學系", "傳播學院碩士學位學程"]); //對不起我累了，有空再補
+    communication[i++] = new node("博士班", ["-----請選擇Choose-----", "新聞學系", "傳播學院博士班"]);
+
+    //外文
+    var foreign_languages = new Array();
+    var i = 0;
+    foreign_languages[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    foreign_languages[i++] = new node("學士班", ["-----請選擇Choose-----", "英國語文學系", "阿拉伯語文學系", "斯拉夫語文學系", "日本語文學系", "韓國語文學系", "土耳其語文學系", "歐洲語文學系", "東南亞語言與文化學士學位學程", "外文中心"]);
+    foreign_languages[i++] = new node("碩士班", ["-----請選擇Choose-----", "英國語文學系", "斯拉夫語文學系", "語言學研究所", "日本語文學系", "韓國語文學系"]); //對不起我累了，有空再補
+    foreign_languages[i++] = new node("博士班", ["-----請選擇Choose-----", "英國語文學系", "語言學研究所"]);
+
+    //法
+    var law = new Array();
+    var i = 0;
+    law[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    law[i++] = new node("學士班", ["法律學系"]);
+    law[i++] = new node("碩士班", ["-----請選擇Choose-----", "法律學系", "法律學系碩士在職專班"]); //對不起我累了，有空再補
+    law[i++] = new node("博士班", ["-----請選擇Choose-----", "法律學系"]);
     //理學院
-    var Science = new Array();
+    var science = new Array();
     var i = 0;
-    Science[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
-    Science[i++] = new node("學士班", ["-----請選擇Choose-----", "應用數學系", "心理學系", "資訊科學系", "數位內容與科技學士學位學程"]);
-    Science[i++] = new node("碩士班", ["-----請選擇Choose-----", "應用數學系", "心理學系", "資訊科學系", "神經科學研究所", "應用物理研究所", "資訊科學系碩士在職專班", "數學教學碩士在職專班"]);
-    Science[i++] = new node("博士班", ["-----請選擇Choose-----", "應用數學系", "心理學系", "資訊科學系", "社群網路與人智計算國際研究生博士學位學程"]);
+    science[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    science[i++] = new node("學士班", ["-----請選擇Choose-----", "應用數學系", "心理學系", "資訊科學系", "數位內容與科技學士學位學程"]);
+    science[i++] = new node("碩士班", ["-----請選擇Choose-----", "應用數學系", "心理學系", "資訊科學系", "神經科學研究所", "應用物理研究所", "資訊科學系碩士在職專班", "數學教學碩士在職專班"]);
+    science[i++] = new node("博士班", ["-----請選擇Choose-----", "應用數學系", "心理學系", "資訊科學系", "社群網路與人智計算國際研究生博士學位學程"]);
 
-
-    //lily white
-    var LLlilywhite = new Array();
+    //國際事務
+    var international_affairs = new Array();
     var i = 0;
-    LLlilywhite[i++] = new node("園田海未", ["私たちは未来の花"]);
-    LLlilywhite[i++] = new node("星空凛", ["くるりんMIRACLE", "Beat in Angel"]);
-    LLlilywhite[i++] = new node("東條希", ["もしもからきっと", "純愛レンズ"]);
+    international_affairs[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    international_affairs[i++] = new node("學士班", ["外交學系"]);
+    international_affairs[i++] = new node("碩士班", ["-----請選擇Choose-----", "外交學系", "東亞研究所", "俄羅斯研究所", "日本研究碩士學位學程", "國際研究英語碩士學位學程"]); //對不起我累了，有空再補
+    international_affairs[i++] = new node("博士班", ["-----請選擇Choose-----", "外交學系", "東亞研究所", "日本研究博士學位學程"]);
+
+    //教育
+    var education = new Array();
+    var i = 0;
+    education[i++] = new node("-----請選擇Choose-----", ["-----請選擇Choose-----"]);
+    education[i++] = new node("學士班", ["教育學系"]);
+    education[i++] = new node("碩士班", ["-----請選擇Choose-----", "教育學院", "教育學系", "幼兒教育研究所", "教育行政與政策研究所", "輔導與諮商碩士學位學程", "學校行政碩士在職專班", "資訊科學系碩士在職專班"]);
+    education[i++] = new node("博士班", ["-----請選擇Choose-----", "教育學院", "教育學系"]);
+
 
     //組合名稱跟上方三個陣列的對應關聯
     var Thecourse = new Array();
     var i = 0;
     Thecourse[i++] = new node("-----請選擇Choose-----", Empty);
-    Thecourse[i++] = new node("整開/通識", Integrated_Courses_General_Education);
-    Thecourse[i++] = new node("輔系/學分專班", minor_credit_program);
-    Thecourse[i++] = new node("理學院", Science);
+    Thecourse[i++] = new node("整開／通識", Integrated_Courses_General_Education);
+    Thecourse[i++] = new node("輔系／學分專班", minor_credit_program);
+    Thecourse[i++] = new node("服務／體育／全民國防", service_PE_all_out_defense);
+    Thecourse[i++] = new node("文學院", liberal);
+    Thecourse[i++] = new node("社會科學院", social_science);
+    Thecourse[i++] = new node("商學院", commerse);
+    Thecourse[i++] = new node("傳播學院", communication);
+    Thecourse[i++] = new node("外文學院", foreign_languages);
+    Thecourse[i++] = new node("法學院", law);
+    Thecourse[i++] = new node("理學院", science);
+    Thecourse[i++] = new node("國際事務學院", international_affairs);
+    Thecourse[i++] = new node("教育學院", education);
 
 
     return (Thecourse);
@@ -193,9 +268,6 @@ function updatePath() {
         text1 = column1.options[index1].text;
         text2 = column2.options[index2].text;
         text3 = column3.options[index3].text;
-        thePath = "你目前點取的是" + text1 + "中的" + text2 + "所演唱的" + text3;
-    } else {
-        thePath = "";
     }
-    document.getElementById("path").innerHTML = thePath;
+
 }
