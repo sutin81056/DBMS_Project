7d6ba72e4279cb1d4363f00a0b647febaf91447c
katie5413 <katie87827@gmail.com>
2020-06-25 19:38:21 +0800

    重新上傳

--- course_select/js/timetable.js
+++
@@ -1,9 +0,0 @@
-let Monday = document.getElementsByClassName("Monday");
-let Tuesday = document.getElementsByClassName("Tuesday");
-let Wednesday = document.getElementsByClassName("Wednesday");
-let Thursday = document.getElementsByClassName("Thursday");
-let Friday = document.getElementsByClassName("Friday");
-let Saturday = document.getElementsByClassName("Saturday");
-let Sunday = document.getElementsByClassName("Sunday");
-let week = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday];
-
